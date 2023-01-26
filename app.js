612e3a6e078f6e6e211765d5c9ef0448c2c5e9f0
Patrick <bilshanpatrick@gmail.com>
2023-01-26 20:53:33 +0400

    fruits saved

--- app.js
+++ app.js
@@ -2,19 +2,40 @@ const mongoose = require('mongoose');
 mongoose.set('strictQuery', true);
 //connecting to mongoose
 mongoose.connect('mongodb+srv://cloudconnect:cBh1BDtyfR8dPIB5	@cluster0.tbojdni.mongodb.net/?retryWrites=true&w=majority')
-//Setting the Schema
+// Setting the Schema
 const fruitSchema = new mongoose.Schema({
   name: String,
-  rating : Number,
+  score : Number,
   review :String
 });
 //declaring the name of Collection
-const Alpha = mongoose.model("Bravo",fruitSchema);
+const Fruit = mongoose.model("Fruit",fruitSchema);
 //adding to the Collection
-const Charlie = new Alpha ({
-  name: "testing the collection name",
-  rating: 07,
-  review : "whats the name"
+const apple = new Fruit ({
+  name : "apple",
+  score : 9,
+  review : "crunchy"
 });
-//saving the new insert
-Charlie.save();
+
+const kiwi = new Fruit({
+  name : "kiwi",
+  score : 10,
+  review : "the best fruit"
+});
+const orange = new Fruit({
+  name : "orange",
+  score : 9,
+  review : "Sour"
+});
+const banana = new Fruit({
+  name : "banana",
+  score : 11,
+  review : "the best fruit of all"
+});
+Fruit.insertMany([apple,kiwi,orange,banana], (err)=>{
+  if (err) {
+    console.log(err);
+  } else {
+    console.log("all fruits successfully saved to the fruits DB");
+  }
+})
